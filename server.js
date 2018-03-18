4cbecf05dd3c576cee2079668278768347f35e76
mattBroman <matthew.broman@yahoo.com>
2018-03-18 16:19:54 -0500

    photoupload complete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const express = require('express');
 const app = express();
 const path = require('path')
-const bodyParser = require('body-parser');
+const busboy = require('connect-busboy');
 const pathHtml = 'assets/html/'
 
 function renderHtml(route, response) {
@@ -18,11 +18,30 @@ function renderHtml(route, response) {
     });
 }
 
-app.use(bodyParser({uploadDir:'./uploads'}));
+app.use(busboy());
 
 app.get('/', (req, res) => renderHtml(pathHtml + 'index.html', res));
 
+app.get('/uploads/:name', function(req, res) {
+    let name = req.params.name;
+    res.sendfile('./uploads/' + name);
+});
+
 app.post('/upload', function(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function(feildname, file, filename) {
+        console.log('uploading');
+        fstream = fs.createWriteStream('./uploads/' + 'image.png');
+        file.pipe(fstream);
+    });
+    req.busboy.on('finish', function() {
+        console.log('uploaded');
+    });
+    renderHtml(pathHtml + 'pic.html', res);
+});
+    
+    /*
     let tempPath = req.files.file.path;
     let targetPath = path.resolve('./uploads/image.png');
     if (path.extname(req.files.file.name.toLowerCase() === '.png')) {
@@ -38,5 +57,6 @@ app.post('/upload', function(req, res) {
     }
     renderHtml(pathHtml + 'pic.html', res);
 });
+*/
 
 app.listen(3000, () => console.log('listening on port 3000'));
